e134dd327c0a5fc7e11d41da50bbcb68e4a0079a
thinhnui.dev <thinh.nguyen@pionero.io>
2023-05-25 18:00:13 +0700

    add image

--- src/containers/admin-home/index.tsx
+++ src/containers/admin-home/index.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
 import React from 'react'
 export const AdminHomeContainer = () => {
   return (
     <>
+      <Image src="/images/logo-156.png" alt="" width={500} height={100} />
       <p>long content</p>
       {Array.from({ length: 100 }, (_, index) => (
         <React.Fragment key={index}>
